b2412096f4c73a0905f7af5fa49057e91cbf7e76
adam who <hs15952016403@gmail.com>
2016-08-27 11:14:31 +0800

    增加文章列表及文章详情页面

--- myweb/articles/urls.py
+++ myweb/articles/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import articles_list, articles_list_all
+from .views import articles_list, articles_list_all, article_detail
 
 urlpatterns = [
         url(r'^list/(?P<block_id>\d+)',articles_list),
         url(r'^lists$',articles_list_all),
+        url(r'^detail/(?P<article_id>\d+)',article_detail),
         ]
--- myweb/articles/views.py
+++ myweb/articles/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
+from blocks.models import Blocks
+from .models import Articles
 
 # Create your views here.
 def articles_list(request,block_id):
     block_id = int(block_id)
-    pass
+    block = Blocks.objects.get(id=block_id)
+    blocks_info = Blocks.objects.all().order_by("id")
+    articles_list = Articles.objects.filter(block=block).order_by("-id")
+    return render(request,"articles_list.html",{"articles":articles_list,"blocks":blocks_info})
 
 
 def articles_list_all(request):
-    return render(request,"articles_list_all.html")
+    blocks_info = Blocks.objects.all().order_by("id")
+    articles_info = Articles.objects.all().order_by("-id")
+    return render(request,"articles_list_all.html",{"blocks":blocks_info,"articles":
+        articles_info})
+
+
+def article_detail(request,article_id):
+    article_id = int(article_id)
+    blocks_info = Blocks.objects.all().order_by("id")
+    article = Articles.objects.get(id=article_id)
+    return render(request,"article_detail.html",{"blocks":blocks_info,"article":article})
